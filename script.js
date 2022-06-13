c69c28f9b32cd153286c06264aaf4515afd855e7
HeyMahAN <hemanmagar169@gmail.com>
2022-06-13 22:25:08 +0545

    Changes done according to M1-W2-D5-Classwork code

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ function experience() {
     var drp = document.querySelector('.about-drop-content');
     drp.classList.toggle ('show');
 }
+function everything(){
+    var everything = document.querySelector('.for-everything').classList;
+    alert('content')
+}
+function arts(){
+    
+}
+function creative(){
+    
+}
+function design(){
+    
+}
